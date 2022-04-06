a930716c3d8b8087fc2dc36691639e2c3e05cd0c
Niels Bosman <nielsbosman@mail.com>
2022-04-06 19:25:06 +0200

    Cleaned up files

--- Services/FetchCoinsService.cs
+++ Services/FetchCoinsService.cs
@@ -7,23 +7,36 @@ public class FetchCoinsService : BackgroundService
     private const string ApiBase = "https://api.coingecko.com/api/";
     private const string ApiVersion = "3";
     private const string ApiUri = $"{ApiBase}v{ApiVersion}";
-    
-    private static readonly HttpClient Http = new();
-    public static event Func<List<Coin>, Task> UpdateEvent;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    private const string Currency = "eur";
+    private const string Order = "market_cap_desc";
+    private const string PerPage = "250";
+    private const string PriceChangePercentage = "1h%2C24h%2C7d";
+
+    private static readonly HttpClient Http = new();
+    public static event Func<List<Coin>, Task>? UpdateEvent;
+
+    /// <summary>
+    /// Excecute the task every n seconds.
+    /// </summary>
+    /// <param name="token"></param>
+    protected override async Task ExecuteAsync(CancellationToken token)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        while (!token.IsCancellationRequested)
         {
-            await Task.Delay(TimeSpan.FromSeconds(3), stoppingToken);
+            await Task.Delay(TimeSpan.FromSeconds(2), token);
             var coins = await FetchCoins();
-            await UpdateEvent.Invoke(coins);
+            if (UpdateEvent != null) await UpdateEvent.Invoke(coins);
         }
     }
 
+    /// <summary>
+    /// Fetch coins from CoinGecko API.
+    /// </summary>
+    /// <returns></returns>
     private static async Task<List<Coin>> FetchCoins()
     {
-        const string endpoint = $"{ApiUri}/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&price_change_percentage=1h%2C24h%2C7d";
+        const string endpoint = $"{ApiUri}/coins/markets?vs_currency={Currency}&order={Order}&per_page={PerPage}&price_change_percentage={PriceChangePercentage}";
         var response = await Http.GetFromJsonAsync<List<Coin>>(endpoint);
         return response ?? new List<Coin>();
     }
